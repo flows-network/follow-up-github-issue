8b72077287bda21e9aac649c08e13197d41c7ad8
Ji Chen <jaykchen@gmail.com>
2023-04-07 22:01:39 -0400

    it compiles

--- src/lib.rs
+++ src/lib.rs
@@ -1,90 +1,84 @@
 use chrono::{DateTime, Datelike, Duration, Utc};
-use github_flows::get_octo;
+use github_flows::{
+    get_octo, listen_to_event, octocrab::models::events::payload::PullRequestEventAction,
+    EventPayload,
+};
 use http_req::uri::Uri;
-// use openai_flows::encode;
-use schedule_flows::schedule_cron_job;
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
 use serde_json::Value;
 use slack_flows::send_message_to_channel;
-use urlencoding::encode;
+use dotenv::dotenv;
+use std::env;
 #[no_mangle]
-pub fn run() {
-    schedule_cron_job(
-        String::from("36 * * * *"),
-        String::from("cron_job_evoked"),
-        callback,
-    );
+#[tokio::main(flavor = "current_thread")]
+pub async fn run()  {
+    dotenv().ok();
+
+    let login: String = match env::var("login") {
+        Err(_) => "jaykchen".to_string(),
+        Ok(name) => name,
+    };
+
+    let owner: String = match env::var("owner") {
+        Err(_) => "jaykchen".to_string(),
+        Ok(name) => name,
+    };
+
+    let repo: String = match env::var("repo") {
+        Err(_) => "vitesse-lite".to_string(),
+        Ok(name) => name,
+    };
+
+    listen_to_event(&login, &owner, &repo, vec!["pull_request", "issues", "issue_comment"], |payload| {
+        handler(&login, &owner, &repo, payload)
+    })
+    .await;
+
 }
 
-// let github_token = "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".to_string();
-
-fn callback(_body: Vec<u8>) {
-    let login = "jaykchen";
-    let owner = "jaykchen";
-    let repo = "vitesse-lite";
-    let search_key_word = "GitHub WASMEDGE";
-    let mut writer = Vec::new();
-
+async fn handler(login: &str, owner: &str, repo: &str, payload: EventPayload) {
     let octocrab = get_octo(Some(String::from(login)));
 
-    // let instance = octocrab.
-    // q=windows+label:bug+language:python+state:open+comments:0+updated:<YYYY-MM-DD&sort=created&order=asc
+    let search_key_word = "GitHub";
+    let query_params = format!("q={search_key_word}+is:issue+state:open+comments:0+updated:<2023-03-18");
+    let response = octocrab
+        .search()
+        .issues_and_pull_requests(&query_params)
+        // .since("2023-03-18")
+        .sort("created")
+        .order("desc")
+        .send()
+        .await;
 
-    // let query_params = format!("q={search_keyword}+repo:jaykchen/vitesse-lite+is:issue+state:open+comments:0+updated:<2023-03-18&sort=created&order=asc");
+    match response {
+        Err(_e) => {}
 
-    // let query_params: Value = serde_json::json!({
-    //     "q": search_key_word,
-    //     "sort": "created",
-    //     "order": "desc"
-    // });
-    let query_params = encode("q=windows+label:bug+language:python+state:open+comments:0+updated:<2023-03-18&sort=created&order=asc");
-    // let query_string = (&query_params).unwrap();
-    let url_str = format!("https://api.github.com/search/issues?{}", query_params);
+        Ok(search_result) => {
+            let now = Utc::now();
+            let one_week_earlier = now - Duration::days(7);
+            for item in search_result.items {
+                let name = item.user.login;
+                let title = item.title;
+                let html_url = item.html_url;
+                let time = item.created_at;
 
-    let url = Uri::try_from(url_str.as_str()).unwrap();
+                if time > one_week_earlier {
+                    // let text = format!(
+                    //     "{name} mentioned WASMEDGE in issue: {title}  @{html_url}\n{time}"
+                    // );
 
-    // match Request::new(&url)
-    //     .method(Method::GET)
-    //     .header("User-Agent", "flows-network connector")
-    //     .header("Content-Type", "application/vnd.github.v3+json")
-    //     .send(&mut writer)
-    // {
-    //     Ok(res) => {
-    //         if !res.status_code().is_success() {}
-    //         let response: Result<SearchResult, _> = serde_json::from_slice(&writer);
-    //         match response {
-    //             Err(_e) => {}
+                    let data = serde_json::json!({
+                        "Name": name,
+                        "Repo": html_url,
+                        "Created": time,
+                    });
 
-    //             Ok(search_result) => {
-    //                 let now = Utc::now();
-    //                 // let one_hour_ago = now - Duration::minutes(60);
-    //                 let one_day_earlier = now - Duration::minutes(1440);
-    //                 for item in search_result.items {
-    //                     let name = item.user.login;
-    //                     let title = item.title;
-    //                     let html_url = item.html_url;
-    //                     let time = item.created_at;
-    //                     let parsed = DateTime::parse_from_rfc3339(&time).unwrap_or_default();
+                    send_message_to_channel("ik8", "ch_out", data.to_string());
+                }
+            }
+        }
+    };
 
-    //                     if parsed > one_day_earlier {
-    //                         // let text = format!(
-    //                         //     "{name} mentioned WASMEDGE in issue: {title}  @{html_url}\n{time}"
-    //                         // );
-
-    //                         let data = serde_json::json!({
-    //                             "Name": name,
-    //                             "Repo": html_url,
-    //                             "Created": time,
-    //                         });
-
-    //                         send_message_to_channel("ik8", "ch_out", data.to_string());
-    //                     }
-    //                 }
-    //             }
-    //         }
-    //     }
-    //     Err(_e) => {}
-    // }
 }
 
 #[derive(Debug, Deserialize)]
